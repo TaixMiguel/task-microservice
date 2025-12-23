37f0cb92199c70ed4d048414c38a5b2477bfb5df
TaixMiguel <taixmiguel@none.com>
2025-12-23 16:44:20 +0100

    feat: Add REST API layer with validation and integration tests

---
+++ tasks-manager/src/main/kotlin/com/taixmiguel/penkatur/tasksmanager/infrastructure/rest/TaskApi.kt
@@ -0,0 +1,25 @@
+package com.taixmiguel.penkatur.tasksmanager.infrastructure.rest
+
+import com.taixmiguel.penkatur.tasksmanager.application.dto.TaskRequestDTO
+import com.taixmiguel.penkatur.tasksmanager.application.query.TaskSearchCriteria
+import com.taixmiguel.penkatur.tasksmanager.domain.Task
+import jakarta.validation.Valid
+import jakarta.ws.rs.BeanParam
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+
+@Path("/api/tasks")
+interface TaskApi {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    fun findTasks(@BeanParam criteria: TaskSearchCriteria): List<Task>
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    fun saveTask(@Valid request: TaskRequestDTO): Response
+}
---
+++ tasks-manager/src/main/kotlin/com/taixmiguel/penkatur/tasksmanager/infrastructure/rest/TaskResource.kt
@@ -0,0 +1,21 @@
+package com.taixmiguel.penkatur.tasksmanager.infrastructure.rest
+
+import com.taixmiguel.penkatur.tasksmanager.application.TaskService
+import com.taixmiguel.penkatur.tasksmanager.application.dto.TaskRequestDTO
+import com.taixmiguel.penkatur.tasksmanager.application.query.TaskSearchCriteria
+import com.taixmiguel.penkatur.tasksmanager.domain.Task
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Inject
+import jakarta.ws.rs.core.Response
+
+@ApplicationScoped
+class TaskResource @Inject constructor(
+    private val service: TaskService
+): TaskApi {
+    override fun findTasks(criteria: TaskSearchCriteria): List<Task> = service.findTasks(criteria)
+
+    override fun saveTask(request: TaskRequestDTO): Response {
+        service.saveTask(request.toTask())
+        return Response.status(Response.Status.CREATED).build()
+    }
+}
